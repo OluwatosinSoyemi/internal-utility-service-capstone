d513fd291411b6a5ce915ce9f88537fb0d353dd0
Oluwatosin Soyemi <oluwatosinsoyemi9@gmail.com>
2026-03-01 08:46:31 +0000

    Phase 0: tests passing and dependencies

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-from flask import Flask, jsonify
-from database import get_users
-import config
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return jsonify({
-        "message": "Internal Utility Service Running",
-        "environment": config.ENVIRONMENT,
-        "db_host": config.DB_HOST  #leaking config info
-    })
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify({"status": "UP"}), 200
 
-@app.route("/users")
-def users():
-    return jsonify(get_users())
+@app.route('/sum', methods=['POST'])
+def get_sum():
+    data = request.get_json()
+    result = data.get('a', 0) + data.get('b', 0)
+    return jsonify({"result": result})
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)  #debug mode ON
+@app.route('/reverse-string', methods=['POST'])
+def reverse_string():
+    data = request.get_json()
+    text = data.get('text', "")
+    return jsonify({"result": text[::-1]})
+
+if __name__ == '__main__':
+    app.run()
