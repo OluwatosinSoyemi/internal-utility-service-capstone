ad9febc94650d282483805d101da09d724abc5fe
Oluwatosin Soyemi <oluwatosinsoyemi9@gmail.com>
2026-03-10 01:41:17 +0000

    Fix flake8 issues and remove leaked credentials

--- config.py
+++ config.py
@@ -1,7 +1,1 @@
-#Hardcoded secrets
-DB_HOST = "localhost"
-DB_USER = "admin"
-DB_PASSWORD = "SuperSecretPassword123"
-DB_NAME = "internal_db"
-
 ENVIRONMENT = "development"
--- database.py
+++ database.py
@@ -1,16 +1,11 @@
-import config
-
 def get_users():
-    # Fake database simulation
     return [
         {
             "id": 1,
             "name": "Alice",
-            "db_user": config.DB_USER  #leaking credentials
         },
         {
             "id": 2,
             "name": "Bob",
-            "db_password": config.DB_PASSWORD  #leaking password
         }
     ]
